045aca62cf5b81d62d2507be499762a56368eb9e
Corentin <corentinlesage02@gmail.com>
2020-03-11 22:49:14 +0100

    [Ajout dossier] ajout des tests de communication Back/Front

---
+++ Test Ant Dynamics/jsonresponse/jsonresponse/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+def send_json(request):
+
+    data = [{'name': 'Peter', 'email': 'peter@example.org'},
+            {'name': 'Julia', 'email': 'julia@example.org'}]
+
+    return JsonResponse(data, safe=False)
