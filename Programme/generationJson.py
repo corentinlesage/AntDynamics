6b8fd28a37c5264dbd6b7502f720cbbc360f3a35
Corentin <corentinlesage02@gmail.com>
2020-05-22 19:12:51 +0200

    [ Modification ] fichier generationJson : ajout et modification de la méthode de génération du JSON

--- Programme/generationJson.py
+++ Programme/generationJson.py
@@ -1,6 +1,7 @@
 from Programme.Animal import Animal
 from Programme.Ant import Ant
 from Programme.Anthill import Anthill
+from Programme.Egg import Egg
 from Programme.Element import Element
 from Programme.Environment import Environment
 from Programme.Path import Path
@@ -13,6 +14,7 @@ from Programme.Supply import Supply
 
 
 import copy
+import json
 
 class test():
     def __init__(self):
@@ -27,21 +29,51 @@ def serialiseur_perso(obj):
     if isinstance(obj, Queen):
         obj_cpy = copy.copy(obj)
         obj_cpy.__class__ = Ant
+        return {"__class__": "Queen",
+                "lay_rate": obj.lay_rate,
+                "__parent__": serialiseur_perso(obj_cpy)
+                }
+
 
     if isinstance(obj, Soldier):
         obj_cpy = copy.copy(obj)
         obj_cpy.__class__ = Ant
+        return {"__class__": "Soldier",
+                "__parent__": serialiseur_perso(obj_cpy)
+                }
+
+    if isinstance(obj, Egg):
+        obj_cpy = copy.copy(obj)
+        obj_cpy.__class__ = Ant
+        return {"__class__": "Egg",
+                "hatch": obj.hatch,
+                "__parent__": serialiseur_perso(obj_cpy)
+                }
 
     if isinstance(obj, Worker):
         obj_cpy = copy.copy(obj)
         obj_cpy.__class__ = Ant
+        if (obj.supply[0] == None) :
+            return {"__class__": "Worker",
+                    "supply_capacity": obj.supply[1],
+                    "supply": "nothing",
+                    "__parent__": serialiseur_perso(obj_cpy)
+                    }
+        else:
+            return {"__class__": "Worker",
+                    "supply_capacity": obj.supply[1],
+                    "supply": serialiseur_perso(obj.supply[0]),
+                    "__parent__": serialiseur_perso(obj_cpy)
+                    }
+
 
     if isinstance(obj, Ant):
         obj_cpy = copy.copy(obj)
         obj_cpy.__class__ = Animal
         return {"__class__": "Ant",
                 #"home": obj.home,
-                "age": generationListeEntier(obj.age),
+                "age": obj.age[0],
+                "age_max": obj.age[1],
                 "role": str(obj.role),
                 "__parent__": serialiseur_perso(obj_cpy)
                 }
@@ -49,12 +81,15 @@ def serialiseur_perso(obj):
     if isinstance(obj, Animal):
         return {"__class__": "Animal",
                 #"element": obj.element,
-                "life": generationListeEntier(obj.life),
-                "size": str(obj.size),
-                "damage": str(obj.damage),
-                "hunger": generationListeEntier(obj.hunger),
-                "thirst": generationListeEntier(obj.thirst),
-                "is_travelling": str(obj.is_travelling)}
+                "life": obj.life[0],
+                "life_max:": obj.life[1],
+                "size": obj.size,
+                "damage": obj.damage,
+                "hunger": obj.hunger[0],
+                "hunger_max": obj.hunger[1],
+                "thirst": obj.thirst[0],
+                "thirst_max": obj.thirst[1],
+                "is_travelling": obj.is_travelling}
 
     if isinstance(obj, Anthill):
         return {"__class__": "Anthill",
@@ -67,7 +102,8 @@ def serialiseur_perso(obj):
     if isinstance(obj, Element):
         return {"__class__": "Element",
                 "radius": obj.radius,
-                "capacity": generationListeEntier(obj.capacity),
+                "capacity": obj.capacity[0],
+                "capacity_max": obj.capacity[1],
                 "position": serialiseur_perso(obj.position),
                 "pheromone": serialiseur_perso(obj.pheromone),
                 "list_animal": generationListe(obj.list_animal),
@@ -85,12 +121,15 @@ def serialiseur_perso(obj):
                 # "start": serialiseur_perso(obj.start),
                 # "end": serialiseur_perso(obj.end),
                 "cost": obj.cost,
-                "capacity": generationListeEntier(obj.capacity)
+                "capacity": obj.capacity[0],
+                "capacity_max": obj.capacity[1],
                 }
 
     if isinstance(obj, Pheromone):
         return {"__class__": "Pheromone",
-                "pheromone": generationListeEntier(obj.pheromone)
+                "pheromone_danger": obj.pheromone[0],
+                "pheromone_food": obj.pheromone[1],
+                "pheromone_recruit": obj.pheromone[2]
                 }
 
     if isinstance(obj, Position):
@@ -108,14 +147,14 @@ def serialiseur_perso(obj):
 
 
 def generationListe(liste):
-    generate = "["
+    generate = list()
     for elem in liste:
-        generate += "{ "+ str(serialiseur_perso(elem)) +" }"
-    return generate + "]"
+        generate.append(serialiseur_perso(elem))
+    return generate
 
 
 def generationListeEntier(liste):
     generate = ""
     for elem in liste:
-        generate +=  str(elem) + " , "
-    return generate[::3]
+        generate += str(elem) + " , "
+    return generate.strip(" , ")
